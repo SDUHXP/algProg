7f1d2d7734c5ef44311abdadf582774b6610e3c1
xiaoping <hxpahb@163.com>
2022-01-23 17:35:40 +0800

    revise solution of A1033; once no gas station at the beginning, travlers can't start

--- 1033 To Fill or Not to Fill .cpp
+++ 1033 To Fill or Not to Fill .cpp
@@ -3,22 +3,24 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
-const int maxN = 505;
-const int inf = 0x3fffffff;
-vector<pair<float,float>>station;
+vector<pair<double,double>>station;
 int main(){
-    float C,D,A; int N; scanf("%f%f%f%d",&C,&D,&A,&N);
+    double C,D,A; int N;
+    scanf("%lf%lf%lf%d",&C,&D,&A,&N);
     for(int i=0;i<N;i++){
-        float price; int pos;
-        scanf("%f%d",&price,&pos);
+        double price,pos;
+        scanf("%lf%lf",&price,&pos);
         station.push_back(make_pair(pos,price));
     }
-    float range = C*A;
+    double range = C*A;
     station.push_back(make_pair(D,0));
-    sort(station.begin(),station.end(),[](pair<float,float>&a,pair<float,float>&b){return a.first<b.first;});
-    int idx = 0,cur = 0;        //current idx of station index;
-    float remain = 0; //range can reach using remaining petrol
-    float cost = 0;
+    sort(station.begin(),station.end());
+    if(station[0].first){
+        printf("The maximum travel distance = 0.00\0");
+        return 0;
+    }
+    int idx,cur = 0;        //current idx of station index;
+    double remain = 0,cost = 0; //range can reach using remaining petrol, and total cost
     while (cur!=station.size()-1){   //while not arrive destination
         if(station[cur+1].first-station[cur].first>range) break;
         int  minIdx = idx = cur+1;
@@ -33,8 +35,8 @@ int main(){
             int flag = cur + 1;
             while(flag != station.size() && station[flag].second>station[cur].second) flag ++;
             cost += (station[flag].first-station[cur].first- remain)/A*station[cur].second;
-            remain = 0;   //range of petrol when reach next target
             cur = flag;
+            remain = 0;   //range of petrol when reach next target
         }
         else{
             cost += (C-remain/A)*station[cur].second;
@@ -46,3 +48,4 @@ int main(){
     else printf("The maximum travel distance = %.2f\n",station[cur].first+range);
     return 0;
 }
+
