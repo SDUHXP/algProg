24b7c544a7703e88da244d9b4031b177bebced5e
xiaoping <hxpahb@163.com>
2022-02-10 15:44:54 +0800

    add solution of A1034 head of gang, two points hasn't pass

--- 1034 Head of a Gang.cpp
+++ 1034 Head of a Gang.cpp
@@ -4,67 +4,68 @@
 #include <vector>
 #include <set>
 using namespace std;
-map<string>name2Id;
-map<int>id2Name;
-int cnt = 0;        //num of existing members
-const int maxN = 1010;
+const int maxN = 2010;
 int relation[maxN][maxN];
 vector<vector<int>>Rec(maxN);
-bool visit[maxN];
+bool visit[maxN] = {0};
 int weight[maxN] = {0};
-int dfs(int beg,int &num,int &head){
+int cnt = 0;        //num of all monitored person
+int dfs(int cur,int &num,int &head){
     int totalW = 0;
-    for(int i=0;i<Rec[beg].size();i++){
-        if(!visit[Rec[beg][i]){
-            visit[rec[beg][i]] = true;
+    for(int i=0;i<Rec[cur].size();i++){
+        int tar = Rec[cur][i];
+        if(relation[cur][tar]){
+            totalW += relation[cur][tar];
+            relation[cur][tar] = relation[tar][cur] = 0;
+        }
+        if(!visit[tar]){
+            visit[tar] = true;
             num ++;
-            totalW += relation[beg][rec[beg][i]];
-            if(weight[beg]>weight[head])  head = beg;
-            totalW  += dfs(Rec[beg][i],num);
+            if(weight[cur]>weight[head])  head = cur;
+            totalW  += dfs(tar,num,head);
         }
     }
     return totalW;
 }
 int main(){
-    int N,K; scanf("%d%d",&N,&K);
+    int N,K,W; scanf("%d%d",&N,&K);
     string str1,str2;
-    int w;
+    map<string,int>name2Id;
+    map<int,string>id2Name;
     for(int i=0;i<N;i++){
-        cin>>str1>>str2>>w;
-        if(!name2Id[str1]){
+        cin>>str1>>str2>>W;
+        if(name2Id[str1]==0){
             name2Id[str1] = ++cnt;
             id2Name[cnt] = str1;
         }
-
-        if(!name2Id[str2]) {
+        if(name2Id[str2]==0) {
             name2Id[str2] = ++cnt;
             id2Name[cnt] = str2;
         }
         int id1 = name2Id[str1];
         int id2 = name2Id[str2];
-        relation[id1][id2] = relation[id2][id1] = w;
-        rec[id1].push_back(id2);
-        rec[id2].push_back(id1);
+        relation[id1][id2] += W;
+        relation[id2][id1] += W;
+        weight[id1] += W;
+        weight[id2] += W;
+        if(relation[id1][id2]>W) continue;
+        Rec[id2].push_back(id1);
+        Rec[id1].push_back(id2);
     }
-    for(int i=1;i<=N;i++){
-        for(int j=0;j<rec[i].size();j++){
-            int tgt = rec[i][j];
-            weight[i] += relation[i][tgt];
+    map<string,int> res;
+    for(int i=1;i<=cnt;i++){
+        if(!visit[i]){
+           int num = 1;
+           int head = i;
+           visit[i] = true;
+           int relation = dfs(i,num,head);        //member id starts from 1;
+           if(relation>K && num>2){
+                string hName = id2Name[head];
+                res[hName] = num;
+            }
         }
     }
-    int grsoup  = 0;
-    set<pair<string,int>> res;
-    for(int i=1;i<=N;i++)
-    if(!visit[i]){
-       int num = 1;
-       int head = i;
-       int relation = dfs(i,&num,&head);        //member id starts from 1;
-       if(relation>K && num>2) group ++;
-       string hName = id2Name[head];
-       res.insert(make_pair(hName,num));
-    }
-    cout<<group<<endl;
-    set<pair<string,id>>::iterator it ;
-    for(it = res.begin();it!=res.end();it++)
-        cout<<it->first<<" "<<it->second<<endl;sss
+    cout<<res.size()<<endl;
+    for(auto it = res.begin();it!=res.end();it++)
+        cout<<it->first<<" "<<it->second<<endl;
 }
