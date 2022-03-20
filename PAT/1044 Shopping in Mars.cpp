fe4da66c16a6d6f8095237794be70855c68fb8b3
xiaoping <hxpahb@163.com>
2022-03-20 14:27:01 +0800

    add solution of A1044 Shopping in Mars

---
+++ PAT/1044 Shopping in Mars.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <algorithm>
+#include <map>
+#include <vector>
+#include <algorithm>
+using namespace std;
+struct interval{
+    int diff;
+    int left,right;
+    interval(int d,int l,int r):diff(d),left(l),right(r){}
+    bool operator<(interval &other){
+        if(diff!=other.diff) return diff<other.diff;
+        else return left<other.left;
+    }
+};
+int binSearch(int arr[],int low,int high,int val){
+    int mid = (low+high)/2;
+    while(low<high){
+        //mid+1 or mid is important, it means result no less than val
+        if(arr[mid]<val) low = mid+1;
+        else high = mid;
+        mid = (low+high)/2;
+    }
+    return low;
+}
+int main(){
+    int N,M; scanf("%d%d",&N,&M);
+    int sum[N+1] = {0};
+    for(int i=1;i<=N;i++) {
+        int val; scanf("%d",&val);
+        sum[i] = sum[i-1] + val;
+    }
+    vector<interval> cache;
+    for(int i=0;i<N;i++){
+        if(sum[N]-sum[i]<M) continue;
+        int pos = binSearch(sum,i+1,N,sum[i]+M);   //[i+1,pos] is ans
+        cache.push_back(interval(sum[pos]-sum[i],i+1,pos));
+    }
+    sort(cache.begin(),cache.end());
+    int diff = cache[0].diff;
+    for(int i=0;i<cache.size();i++)
+        if(cache[i].diff==diff) printf("%d-%d\n",cache[i].left,cache[i].right);
+
+    return 0;
+}
