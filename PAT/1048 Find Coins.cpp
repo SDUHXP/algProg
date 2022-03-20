730141f04e552f66a220e40c8030e16fd1baf1d0
xiaoping <hxpahb@163.com>
2022-03-20 15:02:09 +0800

    add solution of A1048 using binary searching writen by myself

--- PAT/1048 Find Coins.cpp
+++ PAT/1048 Find Coins.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 #include <algorithm>
 using namespace std;
+int binSearch(int arr[],int low,int high,int val){
+    int mid = (low+high)/2;
+    while(low<high){
+        if(arr[mid]==val) return mid;
+        else if(arr[mid]<val) low = mid+1;
+        else high = mid -1;
+        mid = (low+high)/2;
+    }
+    return low;
+}
 int main(){
     int N,M; scanf("%d%d",&N,&M);
     int coin[N+1];
@@ -8,8 +18,8 @@ int main(){
     sort(coin,coin+N);
     for(int i=0;i<N-1;i++){
         if(coin[N-1]<M-coin[i]) continue;
-//        int pos = binSearch(coin,i+1,N-1,M-coin[i]);
-          int pos = lower_bound(coin+i+1,coin+N,M-coin[i]) - coin;
+        int pos = binSearch(coin,i+1,N-1,M-coin[i]);
+//          int pos = lower_bound(coin+i+1,coin+N,M-coin[i]) - coin;
           if(coin[pos]==M-coin[i]){
                 printf("%d %d\n",coin[i],coin[pos]);
                 return 0;
