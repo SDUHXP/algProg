b09bfce45b5cd61d68b53293e0a03d4ed647f275
xiaoping <hxpahb@163.com>
2022-04-22 14:44:13 +0800

    add solution of A1057 Stack using binary indexed arrary

--- PAT/1057 Stack.cpp
+++ PAT/1057 Stack.cpp
@@ -1,3 +1,4 @@
+#if 0
 //using blocking method
 #include <iostream>
 #include <cstring>
@@ -50,3 +51,61 @@ int main(){
     }
     return 0;
 }
+#else
+//using binary indexed tree
+#include <iostream>
+#include <stack>
+#include <string>
+using namespace std;
+const int maxL = 100005;
+stack<int>st;
+int Cnt[maxL]; //Cnt[i] stand for sun of value in [1,i]
+#define lowBit(i)   ((i)&-(i))
+//num of val increase cnt
+void update(int val,int cnt){
+    for(int i=val;i<maxL;i+=lowBit(i)) Cnt[i] += cnt;
+}
+//get order of idx ; that is sum of number no greater than idx
+int getSum(int idx){
+    int ans = 0;
+    for(int i=idx;i>0;i-=lowBit(i)) ans += Cnt[i];
+    return ans;
+}
+//get the value has the order
+int binSearch(int order){
+    int l = 1, r = maxL-1;
+    while(l<r){
+        int mid = (l+r)/2;
+        int cnt = getSum(mid);
+        if(cnt<order) l = mid+1;
+        else if(cnt>=order) r = mid;
+    }
+    return l;
+}
+int main(){
+    int N; scanf("%d",&N);
+    string op; int val;
+    for(int i=0;i<N;i++){
+        cin>>op;
+        if(op=="Push"){
+           cin>>val; st.push(val);
+           update(val,1);
+        }
+        else if (op=="Pop"){
+            if(st.size()==0){printf("Invalid\n"); continue;};
+            cout<<st.top()<<endl;
+            update(st.top(),-1);
+            st.pop();
+        }
+        else{
+            if(st.size()==0){printf("Invalid\n"); continue;};
+            int order = (st.size()+1)/2;
+            int val = binSearch(order);
+            cout<<val<<endl;
+        }
+    }
+    return 0;
+}
+
+
+#endif
