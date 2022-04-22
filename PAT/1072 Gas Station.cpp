f2adf2034a3f4e0faad0a8a96241c6cb31eda8ce
xiaoping <hxpahb@163.com>
2022-04-22 23:49:14 +0800

    change draft of A1072 Gas Station

--- PAT/1072 Gas Station.cpp
+++ PAT/1072 Gas Station.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <cstring>
 using namespace std;
 const int maxN = 1015;
 int graph[maxN][maxN];
-int minDis,int avgDis;
-vector<vector<int>>adjacencyList;
+vector<vector<int>>adjList;
 bool visit[maxN];
 int dis[maxN];          //dis[i] distance from begin to i
 const int inf = 0x3fffffff;
-void dijkstra(int source,int num){
+void dijkstra(int source,int num,float& minDis,float& avgDis){
     fill(dis,dis+maxN,inf);
     dis[source] = 0;
     int trans;
@@ -18,50 +18,52 @@ void dijkstra(int source,int num){
         trans = 0;
         for(int j=1;j<=num;j++)
             if(!visit[i] && dis[j]<dis[trans]) trans = j;
-        for(int i=0;i<adjacencyList[trans].size();i++){
-            int tar = adjacencyList[trans][i];
-            if(dis[tar]>dis[trans]+graph[trans][tar]) dis[tar] = dis[tran] + graph[trans][tar];
+        visit[trans] = true;
+        for(int i=0;i<adjList[trans].size();i++){
+            int tar = adjList[trans][i];
+            if(dis[tar]>dis[trans]+graph[trans][tar]) dis[tar] = dis[trans] + graph[trans][tar];
         }
     }
+    minDis = avgDis = 0;
+    float total = 0;
+    for(int i=1;i<=num;i++){
+        total += dis[i];
+        if(minDis<dis[i]) minDis = dis[i];
+    }
+    avgDis = total/(num-1);
     return;
 }
-void getDis(float &minDis,float& avgDis){
 
-    return ;
-}
 int main(){
     int N,M,K,D; scanf("%d%d%d%d",&N,&M,&K,&D);
-    adjacencyList.resize(N+M+1);
+    adjList.resize(N+M+1);
     for(int i=0;i<K;i++){
         char P1[5],P2[5];
         int st,des,dis; cin>>P1>>P2>>dis;
         if(P1[0]=='G') st = N + atoi(P1+1);
-        else beg = atoi(P1);
+        else st = atoi(P1);
         if(P2[0]=='G') des = N + atoi(P2+1);
         else des = atoi(P2);
         graph[st][des] = graph[des][st] = dis;
-        adjacencyList[st].push_back(des);
-        adjacencyList[des].push_back(st);
+        adjList[st].push_back(des);
+        adjList[des].push_back(st);
 
     }
-    float minAns = -1;
-    float avgAns = -1;
-    int ans = -1;
+    float minAns=0,avgAns; int ans = -1;
     for(int i=1;i<=M;i++){
         memset(visit,0,sizeof(visit));
         memset(dis,0,sizeof(dis));
-        dijkstra(N+i,N+M);
         float minDis = 0, avgDis = 0;
-        getDis(minDis,avgDis);
+        dijkstra(N+i,N+M,minDis,avgDis);
         if(minDis>D) continue;
-        if(minDis<minAns){
-            ans = N + i;
+        if(minDis>minAns){
+            ans = i;
             minAns = minDis;
             avgAns = avgDis;
         }
     }
-    if(minAns<0) printf("No Solution\n");
-    else printf("G%d\n%.1f %.1f\n",ans-N,minAns,avgAns);
+    if(ans<0) printf("No Solution\n");
+    else printf("G%d\n%.1f %.1f\n",ans,minAns,avgAns);
     return 0;
 
 }
