53c6a174e6f9b3438c37fa00405fad6599d7b91c
xiaoping <hxpahb@163.com>
2022-04-23 12:06:19 +0800

    add solution of A1072 Gas station , two point passed; bug need to be fixed

--- PAT/1072 Gas Station.cpp
+++ PAT/1072 Gas Station.cpp
@@ -1,3 +1,5 @@
+//gcc using round half to even;
+//VC++ round half away from zero
 #include <iostream>
 #include <vector>
 #include <algorithm>
@@ -9,28 +11,27 @@ vector<vector<int>>adjList;
 bool visit[maxN];
 int dis[maxN];          //dis[i] distance from begin to i
 const int inf = 0x3fffffff;
-void dijkstra(int source,int num,float& minDis,float& avgDis){
+void dijkstra(int source,int N,int M,int& minDis,int& totalDis){
+    int num = N+M;
     fill(dis,dis+maxN,inf);
-    dis[source] = 0;
-    int trans;
+    dis[source] = 0; int trans;
     //calculate distance from source  (N+M) times
-    for(int i=0;i<num;i++){
+    for(int i=1;i<=num;i++){
         trans = 0;
         for(int j=1;j<=num;j++)
-            if(!visit[i] && dis[j]<dis[trans]) trans = j;
+            if(!visit[j] && dis[j]<dis[trans]) trans = j;
         visit[trans] = true;
-        for(int i=0;i<adjList[trans].size();i++){
-            int tar = adjList[trans][i];
-            if(dis[tar]>dis[trans]+graph[trans][tar]) dis[tar] = dis[trans] + graph[trans][tar];
+        for(int j=0;j<adjList[trans].size();j++){
+            int tar = adjList[trans][j];
+            if(!visit[tar] &&  dis[tar]>dis[trans]+graph[trans][tar])
+                dis[tar] = dis[trans] + graph[trans][tar];
         }
     }
-    minDis = avgDis = 0;
-    float total = 0;
-    for(int i=1;i<=num;i++){
-        total += dis[i];
-        if(minDis<dis[i]) minDis = dis[i];
+    totalDis = 0;
+    for(int i=1;i<=N;i++){
+          totalDis += dis[i];
+          if(dis[i]<minDis) minDis = dis[i];
     }
-    avgDis = total/(num-1);
     return;
 }
 
@@ -49,21 +50,19 @@ int main(){
         adjList[des].push_back(st);
 
     }
-    float minAns=0,avgAns; int ans = -1;
+    float minAns=0, totalAns; int ans = -1;
     for(int i=1;i<=M;i++){
         memset(visit,0,sizeof(visit));
-        memset(dis,0,sizeof(dis));
-        float minDis = 0, avgDis = 0;
-        dijkstra(N+i,N+M,minDis,avgDis);
+        int minDis = inf; int totalDis = 0;
+        dijkstra(N+i,N,M,minDis,totalDis);
+       // printf("minDis = %.1f  avgDis = %.1f \n",minDis,avgDis);
         if(minDis>D) continue;
         if(minDis>minAns){
-            ans = i;
-            minAns = minDis;
-            avgAns = avgDis;
+            ans = i; minAns = minDis; totalAns = totalDis;
         }
     }
     if(ans<0) printf("No Solution\n");
-    else printf("G%d\n%.1f %.1f\n",ans,minAns,avgAns);
+    else printf("G%d\n%.1f %.1f\n",ans,minAns,(totalAns/N));
     return 0;
 
 }
