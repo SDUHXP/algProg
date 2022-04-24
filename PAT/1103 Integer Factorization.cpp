5bac939a67ccde28a16569833f374ba9156f92ec
xiaoping <hxpahb@163.com>
2022-04-24 20:03:24 +0800

    change solution of A1103 Interger Factorization, all test point passed

--- PAT/1103 Integer Factorization.cpp
+++ PAT/1103 Integer Factorization.cpp
@@ -2,36 +2,44 @@
 using namespace std;
 vector<int>path,ans;
 int facSumMax = 0;
-//force deep search
+//violent deep search
+void prtPath(vector<int>&vec){
+    cout<<"path: ";
+    for(auto it=vec.begin();it!=vec.end();it++)
+        cout<<*it<<" ";
+    cout<<endl;
+}
 void dfs(int ele,int reSum,int reNum,int facSum,int tar,int P){
-
-    if(!(reSum&&reNum) && (reSum||reNum)) return;
-    if((reSum||reNum)==0){
+    if((reSum==0)&&(reNum==0)){
         if(facSum>facSumMax){
             facSumMax = facSum;
             ans = path;
         }
+//        prtPath(path);
         return;
     }
+    if(reSum<=0 || reNum<=0) return;
+    if(ele==0) return;
     int tempVal  = pow(ele,P);
-    if(tempVal>tar) return;
     if(reSum>=tempVal){
         path.push_back(ele);
         dfs(ele,reSum-tempVal,reNum-1,facSum+ele,tar,P);   //take element[ele]
         path.pop_back();
     }
-    dfs(ele+1,reSum,reNum,facSum,tar,P);
+    dfs(ele-1,reSum,reNum,facSum,tar,P);
     return;
 }
 int main(){
     int N,K,P; scanf("%d%d%d",&N,&K,&P);
-    dfs(1,N,K,0,N,P);
+    int beg = 21;
+    while(pow(beg,P)>N) beg--;
+    dfs(beg,N,K,0,N,P);
     if(ans.size()==0){
         printf("Impossible\n");
         return 0;
     }
     printf("%d =",N);
-    for(auto it = ans.rbegin();it!=ans.rend();it++)
-        printf(" %d^%d%s",*it,P,(it!=ans.rend()-1?" +":""));
+    for(auto it = ans.begin();it!=ans.end();it++)
+        printf(" %d^%d%s",*it,P,(it!=ans.end()-1?" +":""));
     return 0;
 }
