65ce3fc71916fdb65969b6267a5c57d19724ca16
xiaoping <hxpahb@163.com>
2022-04-24 15:07:25 +0800

    change solution of A1103, two point not pass

--- PAT/1103 Integer Factorization.cpp
+++ PAT/1103 Integer Factorization.cpp
@@ -3,7 +3,8 @@ using namespace std;
 vector<int>path,ans;
 int facSumMax = 0;
 //force deep search
-void dfs(int ele,int reSum,int reNum,int facSum,int K){
+void dfs(int ele,int reSum,int reNum,int facSum,int tar,int P){
+
     if(!(reSum&&reNum) && (reSum||reNum)) return;
     if((reSum||reNum)==0){
         if(facSum>facSumMax){
@@ -12,23 +13,25 @@ void dfs(int ele,int reSum,int reNum,int facSum,int K){
         }
         return;
     }
-    if(reSum>=pow(ele,K)){
+    int tempVal  = pow(ele,P);
+    if(tempVal>tar) return;
+    if(reSum>=tempVal){
         path.push_back(ele);
-        dfs(ele,reSum-pow(ele,K),reNum-1,facSum+ele,K);   //take element[ele]
+        dfs(ele,reSum-tempVal,reNum-1,facSum+ele,tar,P);   //take element[ele]
         path.pop_back();
     }
-    dfs(ele+1,reSum,reNum,facSum,K);
+    dfs(ele+1,reSum,reNum,facSum,tar,P);
     return;
 }
 int main(){
     int N,K,P; scanf("%d%d%d",&N,&K,&P);
-    dfs(1,N,K,0,K);
+    dfs(1,N,K,0,N,P);
     if(ans.size()==0){
         printf("Impossible\n");
         return 0;
     }
-    printf("%d = ",N);
+    printf("%d =",N);
     for(auto it = ans.rbegin();it!=ans.rend();it++)
-        printf("%d^%d + ",*it,P);
+        printf(" %d^%d%s",*it,P,(it!=ans.rend()-1?" +":""));
     return 0;
 }
