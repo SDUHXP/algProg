671623e5c2cf2df8a299d2247532f6d6fcf06086
xiaoping <hxpahb@163.com>
2022-04-29 22:27:36 +0800

    revise not pass file record

--- PAT/1107 Social Clusters.cpp
+++ PAT/1107 Social Clusters.cpp
@@ -38,9 +38,75 @@ int main(){
     for(int i=1;i<=N;i++)   res[findRoot(i)] ++;
     printf("%d\n",res.size());
     set<int,greater<int>> ans;
-    for(auto it = res.begin();it != res.end();it++)
-        ans.insert(it->second);
-    for(auto it=ans.begin();it!=ans.end();it++)
-        printf("%s%d",(it==ans.begin()?"":" "),*it);
+    for(auto it=res.begin();it!=res.end();it++)     ans.insert(it->second);
+    for(auto it=ans.begin();it!=ans.end();it++)     printf("%s%d",(it==ans.begin()?"":" "),*it);
     return 0;
 }
+
+
+#ifdef USE_CORRECT_ANS
+
+#include<cstdio>
+#include<vector>
+#include<algorithm>
+using namespace std;
+int s[1001];
+void Union(int root1,int root2)
+{
+	if(s[root1]<s[root2]){
+		s[root1] += s[root2];
+		s[root2] = root1;
+	}
+	else{
+		s[root2] += s[root1];
+		s[root1] = root2;
+	}
+}
+int Find(int X)
+{
+	if(s[X]<0) return X;
+	else return s[X] = Find(s[X]);
+}
+bool cmp(int a, int b)
+{
+	return a>b;
+}
+int main()
+{
+	fill(s,s+1001,-1);
+	int n,k,root1,root2,h;
+	int hobby[1001];
+	fill(hobby,hobby+1001,0);
+	scanf("%d",&n);
+	for(int i=1; i<=n; i++){
+		scanf("%d:",&k);
+		for(int j=0; j<k; j++){
+			scanf("%d",&h);
+			if(hobby[h]==0){
+				hobby[h] = i;
+			}
+			else{
+				root1 = Find(hobby[h]);
+				root2 = Find(i);
+                if(root1!=root2)
+				    Union(root1,root2);
+			}
+		}
+	}
+	vector<int> v;
+	for(int i=1; i<=n; i++){
+		if(s[i]<=-1){
+			v.push_back(-s[i]);
+		}
+	}
+	sort(v.begin(),v.end(),cmp);
+	printf("%d\n",v.size());
+	for(int i=0; i<v.size(); i++){
+		printf("%d",v[i]);
+		if(i!=v.size()-1) printf(" ");
+		else printf("\n");
+	}
+	return 0;
+}
+
+#endif // CORRECT
--- PAT/README.md
+++ PAT/README.md
@@ -1,3 +1,4 @@
 # PAT solutions to ZJU PAT contest
 A1026 Table Tennis one point not pass
 A1107 Social Clusters one point not pass
+A1053 Path of Equal Weight, one point not passw
