f04473a73a8cff1e17e269653e0715ce4afe4e5e
xiaoping <hxpahb@163.com>
2022-04-30 15:18:24 +0800

    add solution of A1065 A+B and C (64bit), draft version

---
+++ PAT/1065 A+B and C (64bit).cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int T; scanf("%d",&T);
+    long long A,B,C;
+    for(int i=1;i<=T;i++){
+        bool flag = false;
+        scanf("%lld%lld%lld",&A,&B,&C);
+        printf("Case #%d: ",i);
+        if(A>0 && B>0 && (A+B<0)) flag = true;
+        else if (A<0 && B<0 &&(A+B>0)) flag = false;
+        else  flag = (A+B>C);
+        if(flag) printf("true\n");
+        else printf("false\n");
+    }
+    return 0;
+}
