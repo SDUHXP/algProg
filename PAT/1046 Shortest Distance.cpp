e8ba293501069630e98aa3e42faedd09df71d4d2
xiaoping <hxpahb@163.com>
2022-04-30 13:51:19 +0800

    revise solution of A1046 Shortest Distance, all test point passed

--- PAT/1046 Shortest Distance.cpp
+++ PAT/1046 Shortest Distance.cpp
@@ -1,17 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
 const int maxN = 100005;
-int dis[maxN];
+int gap[maxN],dis[2*maxN];
 int main(){
     int N; scanf("%d",&N);
-    for(int i=1;i<=N;i++) scanf("%d",&dis[i]);
-    int totDis = accumulate(dis+1,dis+N+1,0);
+    for(int i=1;i<=N;i++) {scanf("%d",&gap[i]); dis[i+1] = dis[i] + gap[i]; }
     int M; scanf("%d",&M);
     for(int i=0;i<M;i++){
         int st,des; scanf("%d%d",&st,&des);
-        int init = 0;
-        int dis1 = accumulate(dis+min(st,des),dis+max(st,des),0);
-        cout<<min(dis1,totDis-dis1)<<endl;
+        if(st>des) swap(st,des);
+        int dis1 = dis[des]-dis[st];
+        cout<<min(dis1,dis[N+1]-dis1)<<endl;
     }
     return 0;
 }
