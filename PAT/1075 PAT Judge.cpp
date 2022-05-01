26e2d6087cc069f4f709ace2f030d0d76a0ff20e
xiaoping <hxpahb@163.com>
2022-05-01 10:19:10 +0800

    revise solution of A1075,all test point passed

--- PAT/1075 PAT Judge.cpp
+++ PAT/1075 PAT Judge.cpp
@@ -5,7 +5,6 @@ struct user{
     int id;
     int score[6];
     int totScore,passNum;
-    bool visible;
     bool operator<(const user&oth){
         if(totScore!=oth.totScore) return totScore>oth.totScore;
         else if(passNum!=oth.passNum) return passNum>oth.passNum;
@@ -13,7 +12,6 @@ struct user{
     }
     user(){
         for(int i=0;i<6;i++)  score[i] = -2;
-        visible = false;
         totScore = -1;
         passNum = 0;
     }
@@ -24,20 +22,23 @@ int main(){
     for(int i=1;i<=K;i++)  scanf("%d",&fullScore[i]);
     for(int i=0;i<M;i++){
         int uId,pId,pS; scanf("%d%d%d",&uId,&pId,&pS);
-        if(fullScore[pId]==pS && rcds[uId].score[pId]!=pS) rcds[uId].passNum ++;
-        rcds[uId].score[pId] = max(pS,rcds[uId].score[pId]);
+        if(fullScore[pId]==pS && rcds[uId-1].score[pId]!=pS) rcds[uId-1].passNum ++;
+        rcds[uId-1].score[pId] = max(pS,rcds[uId-1].score[pId]);
+        rcds[uId-1].id = uId;
     }
     for(int i=0;i<N;i++)
-        for(int k=0;i<K;k++) if(rcds[i].score[k]>=0) rcds[i].totScore =max(rcds[i].totScore,0)+rcds[i].score[k];
+        for(int k=1;k<=K;k++)
+            if(rcds[i].score[k]>=0) rcds[i].totScore =max(rcds[i].totScore,0)+rcds[i].score[k];
     sort(rcds,rcds+N);
     int rank = -1,preScore= -1;
     for(int i=0;i<N;i++){
         if(rcds[i].totScore<0) break;
         if(rcds[i].totScore!=preScore)  rank = i+1;
-        printf("%d %05d\n",rank,rcds[i].id);
-        for(int k=1;i<=K;k++)
-            if(rcds[i].score[k]!=-1) printf(" %d",rcds[i].score[k]);
+        printf("%d %05d %d",rank,rcds[i].id,rcds[i].totScore);
+        for(int k=1;k<=K;k++)
+            if(rcds[i].score[k]!=-2) printf(" %d",max(rcds[i].score[k],0));
             else printf(" -");
+        printf("\n");
         preScore = rcds[i].totScore;
     }
     return 0;
