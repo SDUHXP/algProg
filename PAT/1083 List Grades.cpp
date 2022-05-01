b1250e9db19cf2a572ea384a7d23034fcc4610b2
xiaoping <hxpahb@163.com>
2022-05-01 16:09:41 +0800

    revise solution of A1083 List Grades,all test point passed

--- PAT/1083 List Grades.cpp
+++ PAT/1083 List Grades.cpp
@@ -12,7 +12,7 @@ int main(){
     for(int i=0;i<N;i++) scanf("%s%s%d",rcds[i].name,rcds[i].id,&rcds[i].sc);
     int lower,upper;    scanf("%d%d",&lower,&upper);
     sort(rcds,rcds+N);
-    if(rcds[0].sc>upper || rcds[N-1].sc<lower) {printf("NONE\ ");return 0;}
+    if(rcds[0].sc<lower || rcds[N-1].sc>upper) {printf("NONE\n");return 0;}
     for(int i=0;i<N;i++)
         if(rcds[i].sc>=lower && rcds[i].sc<=upper)
             printf("%s %s\n",rcds[i].name,rcds[i].id);
