32b537e758f30d688a5940c14718b2b1ec6de5a7
xiaoping <hxpahb@163.com>
2022-05-01 15:46:13 +0800

    add solution of A1083 List Grades, draft version

---
+++ PAT/1083 List Grades.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int maxN = 100005;
+struct stu{
+    char name[15];
+    char id[15];
+    int sc;
+    bool operator<(const stu&oth){return sc>oth.sc;}
+}rcds[maxN];
+int main(){
+    int N; scanf("%d",&N);
+    for(int i=0;i<N;i++) scanf("%s%s%d",rcds[i].name,rcds[i].id,&rcds[i].sc);
+    int lower,upper;    scanf("%d%d",&lower,&upper);
+    sort(rcds,rcds+N);
+    if(rcds[0].sc>upper || rcds[N-1].sc<lower) {printf("NONE\ ");return 0;}
+    for(int i=0;i<N;i++)
+        if(rcds[i].sc>=lower && rcds[i].sc<=upper)
+            printf("%s %s\n",rcds[i].name,rcds[i].id);
+    return 0;
+}
