29bef25b88e04eb1afd14feafb0784a3004ea7bf
xiaoping <hxpahb@163.com>
2022-05-03 13:56:12 +0800

    revise solution ,draft version

--- PAT/1095 Cars on Campus.cpp
+++ PAT/1095 Cars on Campus.cpp
@@ -3,11 +3,12 @@ using namespace std;
 const int maxN = 10001;
 struct record{
     char plate[10];
-    int ts;
-    bool status;    //true for in, and false for out;
+    int ts; bool status;    //true for in, and false for out;
 }rcds[maxN];
 map<string,int>parkTim;
 map<string,int>curStatus;    //record the latest potential valid record index
+map<int,int>parkCnt;
+int carCnt;
 int main(){
     int N,K; scanf("%d%d",&N,&K);
     int h,m,s; char st[5];
@@ -20,10 +21,18 @@ int main(){
     for(int i=0;i<N;i++){
         int idx = curStatus[rcds[i].plate];
         bool flag = rcds[i].status;
-        if(flag==true) curStatus[rcds[i].plate] = i;
+        if(flag==true){
+            curStatus[rcds[i].plate] = i;
+            if(idx==-1) parkCnt[rcds[i].ts] = parkCnt[rcds[idx].ts];
+            else
+            {
+                parkCnt[rcds[idx].ts] --;;
+            }
+        }
         else if(idx>=0 && flag==false){
             parkTim[rcds[i].plate] += rcds[i].ts - rcds[idx].ts;
             curStatus[rcds[i].plate] = -1;
+            parkCnt[rcds[i].ts] = --carCnt;
         }
         else continue;
     }
@@ -36,8 +45,27 @@ int main(){
             maxTim = it->second;
         }
         else if(it->second==maxTim) pltRes.push_back(it->first);
+
+    cout<<endl<<endl;
+    for(auto it=parkCnt.begin();it!=parkCnt.end();it++){
+        int t = it->first;
+        printf("%d:%d:%d %d\n",t/3600,t%3600/60,t%60,it->second);
+    }
+
+    cout<<endl<<endl;
+    while(K--){
+        scanf("%d:%d:%d",&h,&m,&s);
+        int ts = h*3600+m*60+s;
+
+
+    }
+
+    cout<<endl<<endl;
+
     for(int i=0;i<pltRes.size();i++) cout<<pltRes[i]<<" ";
     printf("%02d:%02d:%02d\n",maxTim/3600,maxTim%3600/60,maxTim%60);
+
+    cout<<"carCnt = "<<carCnt<<endl;
 
     return 0;
 }
