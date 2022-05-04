f6d48c9e4764f659a6bf89637ce88c3b2e5f08f9
xiaoping <hxpahb@163.com>
2022-05-04 21:55:05 +0800

    revise solution of A1070,draft version igonre the potential possibility of different cake has same ratio;this version all test point passed;

--- PAT/1070 Mooncake.cpp
+++ PAT/1070 Mooncake.cpp
@@ -1,22 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
+const int maxN = 1005;
+struct cake{
+    double inventory,price;
+    double ratio;
+    bool operator<(cake&oth){return ratio>oth.ratio;}
+}repo[maxN];
 int main(){
     int N,D; scanf("%d%d",&N,&D);
-    float inventory[N],price[N],ratio[N];
-    for(int i=0;i<N;i++) scanf("%f",&inventory[i]);
-    map<float,int> ratio2Id;
+    for(int i=0;i<N;i++) scanf("%lf",&repo[i].inventory);
     for(int i=0;i<N;i++){
-        scanf("%f",&price[i]);
-        ratio2Id[price[i]/inventory[i]] = i;
+        scanf("%lf",&repo[i].price);
+        repo[i].ratio = repo[i].price/repo[i].inventory;
     }
-    sort(ratio,ratio+N,greater<float>());
-    auto it = ratio2Id.rbegin();
-    float profit = 0,re = D;
-    while(re>0){
-        profit += min(re,inventory[it->second])*it->first;
-        re -= inventory[it->second];
-        it++;
+    sort(repo,repo+N);
+    double profit = 0,re = D;
+    for(int i=0;i<N&&re>0;i++){
+        profit += min(re,repo[i].inventory)*repo[i].ratio;
+        re -= repo[i].inventory;
     }
-    printf("%.2f\n",profit);
+    printf("%.2lf\n",profit);
     return 0;
 }
