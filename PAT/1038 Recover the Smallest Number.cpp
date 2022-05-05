302a5d8d89b7c5d6cc90173d30974d10fb0fe962
xiaoping <hxpahb@163.com>
2022-05-05 20:16:19 +0800

    add solution of A1038 Recover the smallest Number, draft version

---
+++ PAT/1038 Recover the Smallest Number.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+vector<string>input;
+int main(){
+    int N; scanf("%d",&N);
+    for(int i=0;i<N;i++){
+        string str; cin>>str;
+        input.push_back(str);
+    }
+    sort(input.begin(),input.end());
+
+    for(int i=0;i<N;i++)
+        if(i==0) cout<<stoi(input[i]);
+        else cout<<input[i];
+    cout<<endl;
+}
