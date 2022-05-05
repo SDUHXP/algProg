25a929990c078db3f3b0fcb5f52c2aa700c05e4e
xiaoping <hxpahb@163.com>
2022-05-06 00:37:36 +0800

    add solution of A1101 Quick Sort, one test point not pass

---
+++ PAT/1101 Quick Sort.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int maxN = 100005;
+int arr[maxN];
+int main(){
+    int N; scanf("%d",&N);
+    for(int i=0;i<N;i++) scanf("%d",&arr[i]);
+    int maxL = -1,minR = 0x3fffffff;
+    int maxLArr[N],minRArr[N];
+    for(int i=0;i<N;i++){
+        if(arr[i]>maxL) maxL = arr[i];
+        maxLArr[i] = maxL;
+    }
+    for(int i=N-1;i>=0;i--){
+        if(arr[i]<minR) minR = arr[i];
+        minRArr[i] = minR;
+    }
+    bool flag = true;
+    set<int>res;
+    for(int i=0;i<N;i++)
+        if(arr[i]>=maxLArr[i] &&arr[i]<=minRArr[i]) res.insert(arr[i]);
+    printf("%d\n",res.size());
+    for(auto it=res.begin();it!=res.end();it++)
+        printf("%d%s",*it,it==(--res.end())?"\n":" ");
+    return 0;
+
+}
