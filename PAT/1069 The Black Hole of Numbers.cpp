b65c5c302390bd9cae39f8284a6454c9e23c548f
xiaoping <hxpahb@163.com>
2022-05-06 13:16:37 +0800

    fix the ignorce of test point 6174; all test point passed

--- PAT/1069 The Black Hole of Numbers.cpp
+++ PAT/1069 The Black Hole of Numbers.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main(){
     int res; cin>>res;
     cout<<setfill('0');
-    while(res!=6174 ){
+    do{
         char tempArr[5]; sprintf(tempArr,"%04d",res);
         sort(tempArr,tempArr+4);
         int minN = atoi(tempArr);
@@ -11,7 +11,6 @@ int main(){
         int maxN = atoi(tempArr);
         res = maxN - minN;
         cout<<setw(4)<<maxN<<" - "<<setw(4)<<minN<<" = "<<setw(4)<<res<<endl;
-        if(res==0) break;
-    }
+    }while(res!=6174 && res!=0);
     return 0;
 }
