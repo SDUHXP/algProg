9cadc09c90543fc4115bfdc723acdfec8989b12f
xiaoping <hxpahb@163.com>
2022-05-06 13:07:59 +0800

    fix the ignorce of test point 0000

--- PAT/1069 The Black Hole of Numbers.cpp
+++ PAT/1069 The Black Hole of Numbers.cpp
@@ -1,20 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
-    string str; cin>>str;
-    int diff = -1;
+    int res; cin>>res;
     cout<<setfill('0');
-    while(diff!=6174 && diff!=0){
-        sort(str.begin(),str.end());
-        int minN = stoi(str);
-        sort(str.begin(),str.end(),[](char a,char b){return a>b;});
-        int maxN = stoi(str);
-        diff = maxN - minN;
-
-        cout<<setw(4)<<maxN<<" - "<<setw(4)<<minN<<" = "<<setw(4)<<diff<<endl;
-        char tempArr[5]; sprintf(tempArr,"%04d",diff);
-        str.assign(tempArr);
-
+    while(res!=6174 ){
+        char tempArr[5]; sprintf(tempArr,"%04d",res);
+        sort(tempArr,tempArr+4);
+        int minN = atoi(tempArr);
+        sort(tempArr,tempArr+4,[](char a,char b){return a>b;});
+        int maxN = atoi(tempArr);
+        res = maxN - minN;
+        cout<<setw(4)<<maxN<<" - "<<setw(4)<<minN<<" = "<<setw(4)<<res<<endl;
+        if(res==0) break;
     }
     return 0;
 }
