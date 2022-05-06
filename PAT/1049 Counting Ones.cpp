ccee0e370da13fde33dd2fbf7c14f01fc35b76de
xiaoping <hxpahb@163.com>
2022-05-07 00:11:18 +0800

    refactor code of A1049; all test point passed

--- PAT/1049 Counting Ones.cpp
+++ PAT/1049 Counting Ones.cpp
@@ -2,19 +2,15 @@
 using namespace std;
 int main(){
     int N; scanf("%d",&N);
-    int  pos = 1;
-    int sum = 0;
-    int orgN = N;
-    while(N){
-        int cur = N%10;
-        int lVal = N/10;
-        int wt = pow(10,pos-1);
-        int rVal = orgN%wt;
-        if(cur==0) sum += lVal*wt;
-        else if(cur==1)  sum += lVal*wt + rVal+1;
-        else sum += (lVal+1)*wt;
-        pos ++;
-        N /= 10;
+    int  posWt = 1,sum = 0;
+    while(N/posWt){
+        int cur = N/posWt%10;
+        int lVal = N/posWt/10;
+        int rVal = N%posWt;
+        if(cur==0) sum += lVal*posWt;
+        else if(cur==1)  sum += lVal*posWt + rVal+1;
+        else sum += (lVal+1)*posWt;
+        posWt *= 10;
     }
     printf("%d\n",sum);
     return 0;
