2bc8cb661c9d0c2e01bb5096652446ed3539cf37
xiaoping <hxpahb@163.com>
2022-05-09 13:31:52 +0800

    revise solution, fix the ignorce of the nearest prime after 10^4 is 10007; fix the ignorce of input N may be

--- PAT/1078 Hashing.cpp
+++ PAT/1078 Hashing.cpp
@@ -1,10 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
-const int maxN = 10005;
+//be careful about the max value since input M in [2,10^4],while the last prime is 10007;
+const int maxN = 10020;
 bool primTable[maxN];
 int calPrime(){
-    for(int i=0;i<maxN;i++) primTable[i] = true;
-    for(int i=2;i*i<maxN;i++){
+    for(int i=2;i<maxN;i++) primTable[i] = true;
+    for(int i=2;i*2<maxN;i++){
         if(primTable[i]==true){
             for(int j=2;i*j<maxN;j++)
                  primTable[i*j] = false;
@@ -16,21 +17,22 @@ bool posSta[maxN];
 int insKey(int val,int M){
     int pos = val%M;
     int dis = 0;
-    while(pos<M && posSta[pos]){
+    while(dis<M && posSta[pos]){
         dis ++;
-        pos = val%M + dis*dis;
+        pos = val+ dis*dis;
+        pos %= M;
     }
-    if(pos<M) posSta[pos] = true;  //true stands for occupied
     return pos;
 }
 int main(){
     calPrime();
     int M,N; scanf("%d%d",&M,&N);
-    for(;M<maxN;M++) if(primTable[M]) break;
+    for(;M<maxN;M++)
+        if(primTable[M]) break;
     for(int i=1;i<=N;i++){
         int key; scanf("%d",&key);
         int pos = insKey(key,M);
-        if(pos<M) printf("%d",pos);
+        if(posSta[pos]==false){printf("%d",pos); posSta[pos]=true;}
         else printf("-");
         if(i!=N) printf(" ");
     }
