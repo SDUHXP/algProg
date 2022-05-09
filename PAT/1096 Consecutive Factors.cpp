56bc8c85e90f1f8765cb7b5a135788aa1fa2cddb
xiaoping <hxpahb@163.com>
2022-05-09 15:35:56 +0800

    revise solution; fix the ignorce of input may be prime

--- PAT/1096 Consecutive Factors.cpp
+++ PAT/1096 Consecutive Factors.cpp
@@ -5,7 +5,7 @@ int main(){
     long long N; scanf("%ld",&N);
     long long sqrN = sqrt(N) + 1;
     for(int i=2;i<=sqrN;i++){
-        int j = i; int tmpN = N;
+        int j = i; long long tmpN = N;
         for(;j<=sqrN;j++){
             if(tmpN%j!=0) break;
             tmpN /= j;
@@ -15,8 +15,11 @@ int main(){
             resInterval.second = j;
         }
     }
-    printf("%d\n",resInterval.second-resInterval.first);
-    for(int i=resInterval.first;i!=resInterval.second;i++)
-        printf("%s%d",i==resInterval.first?"":"*",i);
+     if(resInterval.second!=resInterval.first){
+            printf("%d\n",resInterval.second-resInterval.first);
+            for(int i=resInterval.first;i!=resInterval.second;i++)
+                printf("%s%d",i==resInterval.first?"":"*",i);
+     }
+    else printf("1\n%d",N);
     return 0;
 }
