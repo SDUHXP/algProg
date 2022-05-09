d8f194cd461eaa22f12864f0b5c445aad7151c6f
xiaoping <hxpahb@163.com>
2022-05-09 23:13:16 +0800

    add solution of A1063 ,draft version; not point passed

---
+++ PAT/1063 Set Similarity.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+map<int,set<int>>sets;
+set<int>resSet;
+int main(){
+    int N,val; scanf("%d",&N);
+    for(int i=1;i<=N;i++){
+        int M; scanf("%d",&M);
+        for(int j=0;j<M;j++){
+            scanf("%d",&val);
+            sets[i].insert(val);
+        }
+    }
+    int K; scanf("%d",&K);
+    while(K--){
+        int id1,id2; scanf("%d%d",&id1,&id2);
+        resSet.clear();
+        set_union(sets[id1].begin(),sets[id1].end(),sets[id2].begin(),sets[id2].end(),
+                    inserter(resSet,resSet.begin()));
+        int cnt1 = sets[id1].size();
+        int cnt2 = sets[id2].size();
+        int cnt = resSet.size();
+        printf("cnt1 = %d cnt2 = %d cnt = %d \n",cnt1,cnt2,cnt);
+        printf("%.1f%\n",(cnt1+cnt2-cnt)*100.0/cnt);
+    }
+    return 0;
+}
