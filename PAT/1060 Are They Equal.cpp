d88dbf9b318db92cc3a794fe5672cc3895c4c531
xiaoping <hxpahb@163.com>
2022-05-10 11:27:36 +0800

    revise solution, one test point runtime error; code need to be refactored

--- PAT/1060 Are They Equal.cpp
+++ PAT/1060 Are They Equal.cpp
@@ -4,23 +4,21 @@ string cvtSci(string str,int N){
     int pos = str.find('.');
     int len = str.length();
     string res = "0.";
-    if(pos!=-1){
-        str.erase(pos);
-
-    }
-    res += str.substr(0,N);
+    if(pos!=-1) str.erase(pos,1);
+    else pos = len;
+    int digPos = str.find_first_not_of('0');
+    res += str.substr(digPos,N);
     res += "*10^";
-    res += to_string(pos==-1?len:min(pos,len));
+    res += to_string(pos-digPos);
     return res;
 }
 
 int main(){
     int N; string str1,str2;
     cin>>N>>str1>>str2;
-
+//    cin>>N>>str1;
     string strRes1 = cvtSci(str1,N);
     string strRes2 = cvtSci(str2,N);
-
     if(strRes1==strRes2){
         cout<<"YES ";
         cout<<strRes1<<endl;
@@ -29,8 +27,5 @@ int main(){
         cout<<"NO ";
         cout<<strRes1<<" "<<strRes2<<endl;
     }
-
-
-
     return 0;
 }
