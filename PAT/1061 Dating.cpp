158af048ee9cac99db7815374beaa499920ecd87
xiaoping <hxpahb@163.com>
2022-05-11 13:57:16 +0800

    fix the ignorce of the fist commom char shoulde in [A,G],and the second shole in [A,N]; the common char not in range sholud not be counted

--- PAT/1061 Dating.cpp
+++ PAT/1061 Dating.cpp
@@ -2,18 +2,17 @@
 using namespace std;
 char week[7][5]={"SUN","MON","TUE","WED","THU","FRI","SAT"};
 int main(){
-    string str1,str2;
-    cin>>str1>>str2;
-    int day,hour,minute;
+    string str1,str2; cin>>str1>>str2;
+    int day,hour=-1,minute;
     bool flag = false;
     for(int i=0;i<str1.length()&&i<str2.length();i++)
         if(str1[i]==str2[i]){
-            if(flag==false&&isupper(str1[i])){day = str1[i]-'A'+1;flag=true;}
-            else if(flag==true && isupper(str1[i])) hour = str1[i]-'A'+10;
+            if(flag==false&&str1[i]>='A'&&str1[i]<='G'){day = str1[i]-'A'+1;flag=true;}
+            else if(flag==true && str1[i]>='A'&&str1[i]<='N') hour = str1[i]-'A'+10;
             else if(flag==true && isdigit(str1[i])) hour = str1[i]-'0';
             else continue;
+            if(flag==true && hour!=-1) break;
         }
-
     cin>>str1>>str2;
     for(int i=0;i<str1.length()&&i<str2.length();i++)
         if(str1[i]==str2[i]&&isalpha(str1[i])){
