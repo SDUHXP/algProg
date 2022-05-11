d5e1200140138c33b353b168b67cd4e019b1cd1c
xiaoping <hxpahb@163.com>
2022-05-11 16:47:28 +0800

    simplify solution

--- PAT/1073 Scientific Notation.cpp
+++ PAT/1073 Scientific Notation.cpp
@@ -4,19 +4,15 @@ int main(){
     string str; cin>>str;
     int expPos = str.find('E');
     int exp = stoi(str.substr(expPos+1));
-    str.erase(expPos);
-
+    str.erase(expPos); str.erase(2,1);   //erase punctuation
     if(exp>0){
         int tarPos = 2+exp;
-        str.erase(2,1);
         if(tarPos>str.size()) str.append(tarPos-str.size(),'0');
         if(tarPos<str.size())  str.insert(tarPos,1,'.');
     }
     else if(exp<0){
-        str.erase(2,1);
         str.insert(1,-exp,'0');
         str.insert(2,1,'.');
-
     }
     if(str[0]=='+') str.erase(0,1);
     cout<<str<<endl;
