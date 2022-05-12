d24bd8a0a2c18262e9c76252b68b21398ab957d2
xiaoping <hxpahb@163.com>
2022-05-12 17:22:05 +0800

    revise solution; all test point passed,code need to be refacted

--- PAT/1100 Mars Numbers.cpp
+++ PAT/1100 Mars Numbers.cpp
@@ -1,18 +1,18 @@
 #include <bits/stdc++.h>
-char lower[14][10] = {"tret","jan","feb","mar","apr","may","jun","jly","aug","sep","oct","nov","dec","tam"};
+char lower[14][10] = {"tret","jan","feb","mar","apr","may","jun","jly","aug","sep","oct","nov","dec"};
 char higher[14][10] = {"","tam","hel","maa","huh","tou","kes","hei","elo","syy","lok","mer","jou"};
 using namespace std;
 int main(){
     int N; scanf("%d%*c",&N);
-    string str,res;
+    string str;
     for(int i=0;i<N;i++){
         getline(cin,str);
         if(isdigit(str[0])==true){
-            res.clear();
-            int l = stoi(str)%13;
-            int h = stoi(str)/13;
-            if(h!=0) {res += higher[h]; res += " ";}
-            res += lower[l];
+            string res; int val = stoi(str);
+            int l = val%13; int h = val/13;
+            if(h!=0 && l!=0) {res += higher[h]; res += " "; res += lower[l];}
+            else if(h!=0) res += higher[h];
+            else res += lower[l];
             cout<<res<<endl;
         }
         else{
@@ -24,8 +24,10 @@ int main(){
                     if(strcmp(temp.c_str(),higher[i])==0)  ans += 13*i;
                 str.erase(0,delPos+1);
             }
-            for(int i=0;i<14;i++)
+            for(int i=0;i<13;i++)
                 if(strcmp(str.c_str(),lower[i])==0)  ans += i;
+            for(int i=0;i<13;i++)
+                if(strcmp(str.c_str(),higher[i])==0)  ans += 13*i;
             cout<<ans<<endl;
         }
     }
