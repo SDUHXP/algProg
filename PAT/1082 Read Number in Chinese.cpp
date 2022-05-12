094092fbfddd8dfbac9008e59d8837e021bcbeee
xiaoping <hxpahb@163.com>
2022-05-12 10:43:57 +0800

    reveise solution, draft version; one test point passed

--- PAT/1082 Read Number in Chinese.cpp
+++ PAT/1082 Read Number in Chinese.cpp
@@ -1,16 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
 char dig[10][10] = {"ling","yi","er","san","si","wu","liu","qi","ba","jiu"};
-char wt[5][5] = {"","shi","bai","qian"};
-char seg[3][5] = {"","Wan","Yi"};
+char wt[5][5] = {"","Shi","Bai","Qian"};
+char seg[3][8] = {""," Wan "," Yi "};
 string cvtHan(int val){
     string str = "";
     while(val){
-        if(val>=1000) {str += dig[val/1000]; str += " qian ";}
-        else if(val>=100) {  str += dig[val/100]; str += " bai ";}
-        else if (val>=10) { str += dig[val/10]; str += " shi ";}
-        else str += dig[val];
-        val /= 10;
+        if(val>=1000) {str += dig[val/1000]; str += " Qian "; val %= 1000;}
+        else if(val>=100) {  str += dig[val/100]; str += " Bai "; val %= 100;}
+        else if (val>=10) { str += dig[val/10]; str += " Shi "; val %= 10;}
+        else {str += dig[val]; val = 0; }
     }
     return str;
 }
@@ -22,7 +21,7 @@ int main(){
         if(N/pow(10,exp)){
             res += cvtHan(N/pow(10,exp));
             res += seg[exp/4];
-            N /= pow(10,exp);
+            N %= (int)pow(10,exp);
         }
         exp -= 4;
     }
