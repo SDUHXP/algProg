a2797fc2d6e9989acd01b4edb320e162c452b9a0
xiaoping <hxpahb@163.com>
2022-05-12 17:37:48 +0800

    revise solution; using one char array to store digit

--- PAT/1100 Mars Numbers.cpp
+++ PAT/1100 Mars Numbers.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
-char lower[14][10] = {"tret","jan","feb","mar","apr","may","jun","jly","aug","sep","oct","nov","dec"};
-char higher[14][10] = {"","tam","hel","maa","huh","tou","kes","hei","elo","syy","lok","mer","jou"};
+char marDig[26][10] = {"tret","jan","feb","mar","apr","may","jun","jly","aug","sep","oct","nov","dec",
+                            "tam","hel","maa","huh","tou","kes","hei","elo","syy","lok","mer","jou"};
 using namespace std;
 int main(){
     int N; scanf("%d%*c",&N);
@@ -10,9 +10,9 @@ int main(){
         if(isdigit(str[0])==true){
             string res; int val = stoi(str);
             int l = val%13; int h = val/13;
-            if(h!=0 && l!=0) {res += higher[h]; res += " "; res += lower[l];}
-            else if(h!=0) res += higher[h];
-            else res += lower[l];
+            if(h!=0 && l!=0) {res += marDig[h+12]; res += " "; res += marDig[l];}
+            else if(h!=0) res += marDig[h+12];
+            else res += marDig[l];
             cout<<res<<endl;
         }
         else{
@@ -20,14 +20,12 @@ int main(){
             int delPos = str.find(' ');
             if(delPos!=-1){
                 string temp = str.substr(0,delPos);
-                for(int i=1;i<13;i++)
-                    if(strcmp(temp.c_str(),higher[i])==0)  ans += 13*i;
+                for(int i=13;i<25;i++)
+                    if(strcmp(temp.c_str(),marDig[i])==0)  ans += 13*(i-12);
                 str.erase(0,delPos+1);
             }
-            for(int i=0;i<13;i++)
-                if(strcmp(str.c_str(),lower[i])==0)  ans += i;
-            for(int i=0;i<13;i++)
-                if(strcmp(str.c_str(),higher[i])==0)  ans += 13*i;
+            for(int i=0;i<25;i++)
+                if(strcmp(str.c_str(),marDig[i])==0)  ans += i>12?(i-12)*13:i;
             cout<<ans<<endl;
         }
     }
