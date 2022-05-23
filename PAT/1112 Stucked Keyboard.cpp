02e203000c460bce4605943051230f601ec405bd
xiaoping <hxpahb@163.com>
2022-05-23 11:12:46 +0800

    revise solution of A1112; fixed the output not printed in detected order

--- PAT/1112 Stucked Keyboard.cpp
+++ PAT/1112 Stucked Keyboard.cpp
@@ -2,20 +2,27 @@
 using namespace std;
 int main(){
     int K; scanf("%d",&K);
-    char str[1005],res[1005];
+    char str[1005];
     scanf("%s[^\n]",str);
     int len = strlen(str);
-    set<char> chSet;
-    for(int i=0;i<len;){
+    set<char> chSet,chDel;
+    int i = 0;
+    while(i<len){
         char ch = str[i];
         int j = i;
         while(j<len&&str[j]==ch) j++;
         if((j-i)%K==0) chSet.insert(ch);
-        else if(chSet.find(ch)!=chSet.end()) chSet.erase(ch);
+        else  chDel.insert(ch);
         i = j;
     }
-
-    for(auto ch:chSet){
+    for(auto ch:chDel) chSet.erase(ch);
+    vector<char>chCand;         //this vector to make sure char is stored in detected order;
+    for(int i=0;i<len && chSet.size();i++){
+        char ch = str[i];
+        if(chSet.find(ch)!=chSet.end()) chCand.push_back(ch);
+        chSet.erase(ch);
+    }
+    for(auto ch:chCand){
         printf("%c",ch);
         char pattern[20];
         sprintf(pattern,"(%c)\\1{%d}",ch,K-1);
@@ -24,5 +31,5 @@ int main(){
 
     }
     printf("\n%s",str);
-
+    return 0;
 }
